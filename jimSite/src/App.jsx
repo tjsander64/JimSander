8c8e636ea6d8ed842e4a658e4fdbe1efd50a735d
Tom Sander <thomasjay256@gmail.com>
2024-09-10 23:57:18 -0700

    Add dynamic rendering based on FB storage + state dict (img filename -> download url)

--- jimSite/src/App.jsx
+++ jimSite/src/App.jsx
@@ -1,37 +1,84 @@
 import { useState, useEffect } from 'react';
 import {
-  ref,
+  ref as pref,
   uploadBytes,
   getDownloadURL,
   listAll,
   list,
 } from "firebase/storage";
+
+import { getDatabase, ref, push as firebasePush } from 'firebase/database';
+
 import { storage } from "./firebase";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  const imagesListRef = ref(storage, "Paintings/");
-  const [imageUrls, setImageUrls] = useState([]);
+// TODO: store 
 
-  const [count, setCount] = useState(0);
+// TODO: break out gallery into its own component, don't keep code on main app page
+
+function App() {
+
+  const db = getDatabase();
+  const  galleryDataRef = ref(db, "Artwork/Gallery")
+
+  const imagesListRef = pref(storage, "Paintings/");
+  const [imageUrls, setImageUrls] = useState(new Map());
+
+  // useEffect(() => {
+  //   var replacementMap = new Map();
+  //   listAll(imagesListRef).then((response) => {
+  //     response.items.forEach((item) => {
+  //       getDownloadURL(item).then((url) => {
+  //         console.log(url);
+  //         console.log(pref(storage, url).name);
+  //         replacementMap.set((pref(storage,url).name), url);
+              
+  //         // {console.log("map here")}
+  //         // {console.log(imageUrls)}
+  //       })
+  //     })
+      
+  //   console.log(replacementMap);
+  //   setImageUrls(new Map(replacementMap));
+  //   console.log(imageUrls);
+  //   })
+  // }, []);
 
   useEffect(() => {
-    listAll(imagesListRef).then((response) => {
-      response.items.forEach((item) => {
-        getDownloadURL(item).then((url) => {
-          setImageUrls((prev) => [...prev, url]);
-        })
-      })
-    })
+    const fetchUrls = async () => {
+      var replacementMap = new Map();
+  
+      const response = await listAll(imagesListRef);
+      const urlPromises = response.items.map(async (item) => {
+        const url = await getDownloadURL(item);
+        const fileName = pref(storage, url).name;
+        replacementMap.set(fileName, url);
+      });
+  
+      // Wait for all the getDownloadURL promises to resolve
+      await Promise.all(urlPromises);
+  
+      // Now update the state after the map is fully built
+      console.log(replacementMap);
+      setImageUrls(new Map(replacementMap)); 
+    };
+  
+    fetchUrls();
   }, []);
+  
 
   return (
     <div>
-      <div> Jim Sanders Gallery </div>
+      <div className="h1 mb-2"> Various Artworks </div>
 
-      {imageUrls.map((url) => {
-        return <img src={url} />;
-      })}
+      <div className="">
+        {Array.from(imageUrls.values()).map((url) => {
+          // TEMPORARY: CHANGE 2 RENDER BASED ON RTDB
+          return <img src={url} />;
+        })}
+      </div>
+
     </div>
 
   )
---
+++ jimSite/src/components/card.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export function Card(props) {
+
+    return (
+        <div> Card </div>
+    )
+}
