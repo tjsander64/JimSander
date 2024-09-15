c799909dee70c90e90478804aa5773d9c42673e1
Tom Sander <thomasjay256@gmail.com>
2024-09-14 17:50:17 -0700

    set up rendering route through db

--- jimSite/src/App.jsx
+++ jimSite/src/App.jsx
@@ -7,7 +7,7 @@ import {
   list,
 } from "firebase/storage";
 
-import { getDatabase, ref, push as firebasePush } from 'firebase/database';
+import { getDatabase, ref, onValue, push as firebasePush } from 'firebase/database';
 
 import { storage } from "./firebase";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -20,31 +20,13 @@ import './App.css';
 function App() {
 
   const db = getDatabase();
-  const  galleryDataRef = ref(db, "Artwork/Gallery")
+  const  galleryDataRef = ref(db, "artwork/gallery/");
 
-  const imagesListRef = pref(storage, "Paintings/");
+  const imagesListRef = pref(storage, "thumbnails/");
   const [imageUrls, setImageUrls] = useState(new Map());
 
-  // useEffect(() => {
-  //   var replacementMap = new Map();
-  //   listAll(imagesListRef).then((response) => {
-  //     response.items.forEach((item) => {
-  //       getDownloadURL(item).then((url) => {
-  //         console.log(url);
-  //         console.log(pref(storage, url).name);
-  //         replacementMap.set((pref(storage,url).name), url);
-              
-  //         // {console.log("map here")}
-  //         // {console.log(imageUrls)}
-  //       })
-  //     })
-      
-  //   console.log(replacementMap);
-  //   setImageUrls(new Map(replacementMap));
-  //   console.log(imageUrls);
-  //   })
-  // }, []);
 
+  // retains up-to-date state map (img filename -> download url)
   useEffect(() => {
     const fetchUrls = async () => {
       var replacementMap = new Map();
@@ -60,11 +42,56 @@ function App() {
       await Promise.all(urlPromises);
   
       // Now update the state after the map is fully built
-      console.log(replacementMap);
+      // console.log(replacementMap);
       setImageUrls(new Map(replacementMap)); 
     };
   
     fetchUrls();
+  }, []);
+
+  const [allPiecesArray, setAllPiecesArray] = useState([]);
+
+  // retains up-to-date metadata by piece
+  useEffect(() => {
+    console.log("useeffect for db in state");
+
+    const galleryDataRef = ref(db, "Artwork/Gallery/");
+
+    const diveTest = ref(db, "Artwork/Gallery/0");
+    console.log("ref : " + diveTest);
+    console.log("Gallery data ref:" + galleryDataRef);
+
+    // const unregisterFunction = onValue(diveTest, (snapshot) => {
+    //   const diveVal = snapshot.val();
+    //   console.log(diveVal);
+    // })
+
+    const unregisterFunction = onValue(galleryDataRef, (snapshot) => {
+      console.log("unregisterfunction");
+      const allPiecesObj = snapshot.val();
+
+      if (allPiecesObj) {
+
+        console.log(allPiecesObj);
+
+        const allPiecesKeys = Object.keys(allPiecesObj);
+
+        const allPiecesArray = allPiecesKeys.map((key) => {
+          const singlePieceCopy = {...allPiecesObj[key]};
+          singlePieceCopy.key = key;
+          return singlePieceCopy;
+        });
+        
+        console.log("Updating pieces array in state");
+        setAllPiecesArray(allPiecesArray);
+        console.log(allPiecesArray);
+      } else {
+        console.log("Snapshot empty - no data");
+        setAllPiecesArray([]);
+      }
+    });
+    console.log(imageUrls);
+    return () => {unregisterFunction();}
   }, []);
   
 
@@ -73,9 +100,13 @@ function App() {
       <div className="h1 mb-2"> Various Artworks </div>
 
       <div className="">
-        {Array.from(imageUrls.values()).map((url) => {
+        {/* {Array.from(imageUrls.values()).map((url) => {
           // TEMPORARY: CHANGE 2 RENDER BASED ON RTDB
           return <img src={url} />;
+        })} */}
+
+        {allPiecesArray.map((piece) => {
+          return <img src={imageUrls.get(piece.img_path)}/>
         })}
       </div>
 
